0cec6df9c0100e5c2edc215e6b86f3d973c209ef
osoohynn <tbr335@naver.com>
2026-03-12 13:32:16 +0900

    fix: create submit response 변경
    - Int type. nullable의 score 컬럼 추가

--- src/main/kotlin/me/suhyun/soj/domain/submission/presentation/response/SubmitResponse.kt
+++ src/main/kotlin/me/suhyun/soj/domain/submission/presentation/response/SubmitResponse.kt
@@ -1,3 +1,3 @@
 package me.suhyun.soj.domain.submission.presentation.response
 
-data class SubmitResponse(val submissionId: Long)
+data class SubmitResponse(val submissionId: Long, val score: Int? = null)
